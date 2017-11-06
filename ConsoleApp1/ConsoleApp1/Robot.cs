0816f4cfe0ed94918e23b31768b434233f84fd1b
Luca-PC\Luca <lucasmitsmans@hotmail.com>
2017-11-06 15:07:54 +0100

    Eerste versie afstandsbediening

---
+++ ConsoleApp1/ConsoleApp1/Robot.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Robot
+    {
+        private int richting;
+        
+        public int Richting
+        {
+            get { return Richting; }
+            set { Richting = value; }
+        }
+
+        private int plaats;
+
+        public int Plaats
+        {
+            get { return plaats; }
+            set { plaats = value; }
+        }
+
+
+
+        public Robot(int plaats, int richting)
+        {
+
+        }
+
+        // een stap voorwaarts zetten en positie printen
+        public void zetEenStap()
+        {
+
+        }
+
+        // Stapsgewijs verplaatsen en elke positie printen
+        public void gaNaar(int bestemming)
+        {
+            // 
+        }
+
+
+    }
+}
